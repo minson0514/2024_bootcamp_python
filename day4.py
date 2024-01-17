af07c77299430883673e483d7adf8fa1dc6e4328
minson0514 <minson0514@gmail.com>
2024-01-17 13:33:55 +0900

    0117_6

--- day4.py
+++ day4.py
@@ -1,55 +1,52 @@
-#sequence data structure
-#immutable -> tuple
-#mutable -> index
-
-#list can contain any type of data
-
-#create from a string with split
-#use split() to chop a string into a list
-#get item by indexing [num]
-#gty items by slicing [:]
-#to reverse a list in place, use list.reverse()
+#원소 중에 immutable한 값을 바꾸면 복제 공간에 있는 값을 건드리지 않아서 값이 변경되지 않음
 '''
-subjects = ["C++", "Java", "Python"]
-print(subjects[::-1])
-print(subjects) #원본이 바뀌지는 않음
-#subjects = subjects[::-1] #assign variable to change origin
-subjects.reverse() #use reverse
-print(subjects) #원본을 바꿈
+subjects = ["a","b","c"]
+a = subjects
+b = subjects.copy()
+c = list(subjects)
+d = subjects[:]
+print(subjects, a,b,c,d)
+subjects[1]="x"
+print(subjects,a,b,c,d)
 '''
-#Add an item to the end with append() -> just add an item at the end of the list
-#Add an item by offset with insert() -> 잘 안 쓰임
-#combine list by using extend() or '+'
-#change Items with a slice
-#delete an Item by offset with del
-#delete an Item by vlaue with remove()
-#get an Item by offset and delete it with pop()
-#delete all items with clear
-#find an Item's offset by value with index()
+#문자열 literal을 list(mutable)로 바꾸니까 copy한 값이 변경되게 됨
 '''
-subjects = ["C++", "Java", "Python"]
-subjects = subjects[::-1]
-print(subjects)
-#by offset
-#del subjects[-2] #역방향 지우기
-#del subjects[2]
-#subjects.pop(2)
-
-#by value
-#subjects.remove("Java") #동일 값이 있는 경우 가장 앞쪽 것을 지움
-print(subjects)
+subjects = ["a",["b","c"],"d"]
+a = subjects
+b = subjects.copy()
+c = list(subjects)
+d = subjects[:]
+print(subjects, a,b,c,d)
+subjects[1][1]="x"
+print(subjects,a,b,c,d)
 '''
 
-#test for a value with in (check for existense)-> return type: bool
-#convert a list to a srting with join
+#깊은 복사, 얕은 복사, 참조
+#->변수는 바뀌지만 복제본이 바뀌는 것을 원하지 않으면 deepcopy를 하면 됨
+'''
+import copy
+subjects = ["a",["b","c"],"d"]
+a = subjects
+b = subjects.copy()
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(a) #deep copy한 부분만 바뀌지 않음
+#a를 복사해서 그대로 copy하기 때문에...
+print(subjects, a,b,c,d,e)
+subjects[1][1]="x"
+print(subjects,a,b,c,d,e)
+'''
 
-#reorder item by sort
-subjects = ["C++", "Java","리눅스","5", "Python", "Java", "9","데이터베이스"]
-subjects = subjects[::-1]
-print(subjects)
-subjects.sort() #정방향 정렬
-#한글, 숫자, 영어 순으로 우선순위
-subjects.sort(reverse=False) #역방향 정령
-#리스트 안에 숫자형 있으면 TypeError: '<' not supported between instances of 'str' and 'int'
-copy_subjects = sorted((subjects))
-print(subjects)
+#pythontutor.com 에서 구현한 코드를 가시적으로 확인할 수 있음
+
+#iterate with for and in -> return True and False
+#iterate multiple sequences with zip()
+#->여러 개의 리스트가 있는데 원소의 길이가 서로 다를 때 가장 작은 리스트의 길이만큼만 run
+
+#create a list with a comprehension
+
+#['expression' for 'item' in 'iterable']
+# -> iterable 자료에 순환 가능한 자료형이 나와야함
+# -> 자동으로 append기능이 있음
+
+
