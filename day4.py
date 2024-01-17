e197576c68aca946f9a726457295d5d2039e79c9
minson0514 <minson0514@gmail.com>
2024-01-17 11:42:56 +0900

    0117_3

--- day4.py
+++ day4.py
@@ -2,60 +2,55 @@
 #immutable -> tuple
 #mutable -> index
 
-t1 = (5) #int
-print("type of t2: ", type(t1))
+#list can contain any type of data
 
-#tuple has atleast one comma
-#except empty tuple e.g. t7=()
-#packing example
-t2=5,
-print("type of t2: ", type(t2))
-t3 = (5,)
-print("type of t3: ", type(t3))
-t4 = (5,7)
-print("type of t4: ", type(t4), "print tuple data structure: ", t4)
-t5 = 5,7
-print("type of t5: ", type(t5))
-t6="python", "kim"
-print("type of t6: ", type(t6), "extract component by []: ", t6[1])
-t7 = ()
-print("type of t7: ", type(t7))
-t8=tuple()
-print("type of t8: ", type(t8))
-
-#unpacking example
-subject, professor = t6
-print("unpacking component subject:", subject)
-print("unpacking component professor:", professor)
-
-#unpacking error example
-#ValueError: not enough values to unpack (expected 3, got 2)
+#create from a string with split
+#use split() to chop a string into a list
+#get item by indexing [num]
+#gty items by slicing [:]
+#to reverse a list in place, use list.reverse()
 '''
-a, b, c = t6
-print("unpacking component subject:", subject)
-print("unpacking component professor:", professor)
+subjects = ["C++", "Java", "Python"]
+print(subjects[::-1])
+print(subjects) #원본이 바뀌지는 않음
+#subjects = subjects[::-1] #assign variable to change origin
+subjects.reverse() #use reverse
+print(subjects) #원본을 바꿈
+'''
+#Add an item to the end with append() -> just add an item at the end of the list
+#Add an item by offset with insert() -> 잘 안 쓰임
+#combine list by using extend() or '+'
+#change Items with a slice
+#delete an Item by offset with del
+#delete an Item by vlaue with remove()
+#get an Item by offset and delete it with pop()
+#delete all items with clear
+#find an Item's offset by value with index()
+'''
+subjects = ["C++", "Java", "Python"]
+subjects = subjects[::-1]
+print(subjects)
+#by offset
+#del subjects[-2] #역방향 지우기
+#del subjects[2]
+#subjects.pop(2)
+
+#by value
+#subjects.remove("Java") #동일 값이 있는 경우 가장 앞쪽 것을 지움
+print(subjects)
 '''
 
-#create with tuple()
-#combine tuples by Using +
-#Duplicate Items with * -> similar with 'str * int'
+#test for a value with in (check for existense)-> return type: bool
+#convert a list to a srting with join
 
-#comparison
-t9 = 1, 2, 3
-t10 = 1, 2, 3, 1
-print(t9 == t10) #false
-print(t9 <= t10) #true
-print(t9 > t10) #false
-
-#modify tuple(combine)
-t11 = 4.43,
-print("id of t11:", id(t11))
-t12 = 3.97, 4.1, 3.27
-print(t11 + t12) #combined temporary
-print(t11)
-print("id of t11:", id(t11))
-t11 = t11 + t12 #t11+=t12
-print(t11) #modified
-print("id of modified t11:",id(t11))
-
+#reorder item by sort
+subjects = ["C++", "Java","리눅스","5", "Python", "Java", "9","데이터베이스"]
+subjects = subjects[::-1]
+print(subjects)
+subjects.sort() #정방향 정렬
+#한글, 숫자, 영어 순으로 우선순위
+subjects.sort(reverse=False) #역방향 정령
+#리스트 안에 숫자형 있으면 TypeError: '<' not supported between instances of 'str' and 'int'
+copy_subjects = sorted((subjects))
+print(subjects)
 
