0ce9b7412e82476ae58a9ca195cb28f786187c29
minson0514 <minson0514@gmail.com>
2024-01-17 15:11:11 +0900

    0117_9

--- day4.py
+++ day4.py
@@ -1,33 +1,17 @@
-#dictionary
-#mutable data structure
-sugang = dict(python='kim', cpp='sung', db='kang')
-#TypeError: unhashable type: 'dict' -> {} 로 dict를 묶으면 에러
-
-#get items
-'''
-print(sugang)
-sugang['datastructure'] = "kim"
-print(sugang)
-sugang['datastructure'] = 'park'
-print(sugang) #type: dictionary
-print(sugang['db']) #kang
-print(sugang.get('db')) #kang
-print(sugang.get('opensource')) #None
-print(sugang.get('opensource', 'not exist'))
-'''
-
-#Print items
-'''
-for subjet, professor in sugang.items():
-    print("%s 과목 담당교수는 %s 입니다."%(subjet,professor)) #print key and value
-
-#for k in sugang.keys():
-for k in sugang: #key -> default
-    print(k) #print key
-for v in sugang.values():
-    print(v) #print value
-
-for s in sugang.items():
-    print(s) #print key and value in tuple
-'''
-
+drinks_food = {"위스키": "초콜릿", "와인": "치즈", "소주": "삼겹살", "고량주": "양꼬치"}
+#drink = input(drinks_food.keys())
+drinks_food_keys = list(drinks_food)
+#print(drinks_food_keys)
+while True:
+    menu = input(f'다음 술 중에 고르세요.\n1. {drinks_food_keys[0]}\n2. {drinks_food_keys[1]}\n3. {drinks_food_keys[2]}\n4. {drinks_food_keys[3]}\n5. 종료\nenter here: ')
+    if menu == '1':
+        print(f'{drinks_food_keys[0]}에 어울리는 안주는 {drinks_food[drinks_food_keys[0]]} 입니다.')
+    elif menu == '2':
+        print(f'{drinks_food_keys[1]}에 어울리는 안주는 {drinks_food[drinks_food_keys[1]]} 입니다.')
+    elif menu == '3':
+        print(f'{drinks_food_keys[2]}에 어울리는 안주는 {drinks_food[drinks_food_keys[2]]} 입니다.')
+    elif menu == '4':
+        print(f'{drinks_food_keys[3]}에 어울리는 안주는 {drinks_food[drinks_food_keys[3]]} 입니다.')
+    else:
+        print(f'다음에 또 오세요')
+        break
