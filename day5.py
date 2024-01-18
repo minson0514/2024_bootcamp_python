06e22ce9e3ced7d3ffcc52451cde5d2bad8a3dff
minson0514 <minson0514@gmail.com>
2024-01-18 10:52:03 +0900

    0118_prime number function

--- day5.py
+++ day5.py
@@ -1,18 +1,28 @@
+#prime number
+#new*
+def isprime(n) -> bool: #return type is boolean type(t/f)
+    """
+    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
+    :param n: 판정할 매개변수
+    :return: 소수면 True, 소수가 아니면 False
+    """
+    if n < 2:
+        return False
+    else:
+        i = 2
+        while i*i <= n:
+            if n % i == 0:
+                return False
+            i += 1
+        return  True
+#help(isprime) #우리가 작성한 함수에 대한 설명이 나옴
+#help(len)
+numbers = input("Input first second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
 
-'''
-univ = 'inha university'
-counts_alphabet = {letter: univ.count(letter) for letter in univ} #dictionary comprehension
-print(counts_alphabet) #{'i': 3, 'n': 2, 'h': 1, 'a': 1, ' ': 1, 'u': 1, 'v': 1, 'e': 1, 'r': 1, 's': 1, 't': 1, 'y': 1}
-'''
-
-'''
-univ = 'inha university'
-counts_alphabet = dict()
-for letter in univ:
-    counts_alphabet[letter] = univ.count(letter)
-print(counts_alphabet)
-'''
-
-#assignment ex 8.10
-squares = {n: pow(n,2) for n in range(10)} #key:0~9 value:key^2
-print(squares)
+if n1 > n2:
+    n1, n2 = n2, n1
+for number in range(n1, n2+1):
+    if isprime(number): #함수 호출
+        print(number, end=' ')
