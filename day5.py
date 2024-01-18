f27e4ccad7385ff491764cda875c9856ae425062
minson0514 <minson0514@gmail.com>
2024-01-18 16:38:13 +0900

    0118_8

--- day5.py
+++ day5.py
@@ -1,12 +1,12 @@
-#map
-#lambda
-def squares(n):
-    return n * n
+def my_range(first=0, last=5, step=1):
+    number=first
+    while number<last:
+        yield  number
+        number+=step
+r = my_range()
+print(r, type(r))
 
-even_numbers = [i for i in range(1,51) if i % 2 == 0]
-print(even_numbers)
-print(tuple(map(squares, even_numbers))) #map(함수이름, sequence type 자료구조)
-print(tuple(map(lambda x : x**2, even_numbers))) #lambda 함수이름 : 수식, sequence type 자료구조
-
-z=lambda  x: pow(x,2)
-print(tuple(map(z,even_numbers)))
+for x in r:
+    print(x) #call generater
+for x in r:
+    print(x) #실행 X
